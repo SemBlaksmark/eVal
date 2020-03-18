ee166b74cad7084d799d70156c41c91cd9995e17
Sem Blaksmark <sb@ecapacity.dk>
2020-03-18 08:54:21 +0100

    Added eventNames to call elements

--- ui.js
+++ ui.js
@@ -1,4 +1,7 @@
 const info = Object.fromEntries(location.search.substr(1).split('&').map(entry => entry.split('=')));
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
 let config;
 
 (async () => {
@@ -7,24 +10,8 @@ let config;
   config = await getConfig(info.account, info.profile);
   if (Object.keys(config).length === 0) alert('something\'s wrong');
 
-  chrome.runtime.onMessage.addListener(m => {
-    switch (m.command) {
-      case 'addCall':
-        addCall(m.call);
-        break;
-    }
-  });
-
+  chrome.runtime.onMessage.addListener(messageListener);
   window.addEventListener('load', windowLoaded);
-  function windowLoaded() {
-    let callsEl = document.querySelector('#calls');
-    let layersEl = document.querySelector('#layers');
-    callsEl.addEventListener('click', e => alert(e.target.textContent));
-    callsEl.addEventListener('mouseover', e => {
-      layersEl.style.display = 'flex';
-      layersEl.textContent = e.target.dataset.layers || 'no text';
-    });
-  }
 })();
 
 async function getConfig(account, profile) {
@@ -44,9 +31,53 @@ async function storageFetch(key) {
   });
 }
 
+function messageListener(m) {
+  switch (m.command) {
+    case 'addCall':
+      addCall(m.call);
+      break;
+  }
+}
+
+function windowLoaded() {
+  let callsEl = $('#calls');
+  let layersEl = $('#layers');
+  callsEl.addEventListener('click', e => alert(e.target.textContent));
+  callsEl.addEventListener('mouseover', e => {
+    layersEl.style.display = 'flex';
+    layersEl.textContent = e.target.dataset.layers || 'no text';
+  });
+}
+
 function addCall(call) {
-  var el = document.createElement('button');
-  el.textContent = call.type;
+  var el = makeEl('div', null, ['call', call.type]);
+
+  [call.type, ...getEvents(call)].forEach(text => {
+    el.append(makeEl('div', null, null, text));
+  })
+
   el.dataset.layers = Object.keys(call);
-  document.querySelector('#calls').append(el);
+  $('#calls').append(el);
+}
+
+function makeEl(type, id, classes, text) {
+  var el = document.createElement(type);
+  if (id) el.id = id;
+  if (classes) classes.forEach(c => el.classList.add(c));
+  if (text) el.append(document.createTextNode(text));
+  return el;
+}
+
+function getEvents(call) {
+  if (config.events.isGroup) {
+    return config.keyGroups[config.events.key].keys.reduce((events, key) => {
+      let value = call.dataLayer[key];
+      Array.isArray(value) ? events.push(...value) : events.push(value);
+      return events;
+     }, [])
+  }
+  else {
+    let value = call.dataLayer[key];
+    return Array.isArray(value) ? value : [value];
+  }
 }
