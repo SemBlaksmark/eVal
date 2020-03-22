e43426208df4275fd76b3f011143b36e5489ff17
Sem Blaksmark <sb@ecapacity.dk>
2020-03-22 20:39:27 +0100

    Hide or show groups based on status

--- ui.js
+++ ui.js
@@ -3,6 +3,7 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 let callIndex = 0;
+//let calls = {};
 let config;
 
 (async () => {
@@ -48,78 +49,113 @@ function DOMReady() {
 
 function addCall(call) {
   let id = callIndex++;
-  call.layers = Object.fromEntries(Object.entries(call.layers).map(([id, layer]) =>
-    [id, Object.fromEntries(Object.entries(config.keyGroups).map(([name, groupObj]) =>
-      [name, Object.fromEntries(groupObj.keys.map(key => [key, layer[key]]))]))]));
+  /*calls[id] =*/ processCall(id, call);
 
-  let layerContainerEl = makeEl('div', null, ['layersContainer']);
-  let detailViewFragment = document.createDocumentFragment();
-  layerContainerEl.dataset.callId = id;
-  Object.entries(call.layers).forEach(([layerId, layer]) => {
-    layerContainerEl.append(makeLayerElement(layerId, id));
-    detailViewFragment.append(makeDetailElement(layer, layerId, id));
-  });
+  createDetails(call);
+  createLayers(call);
+  createCall(call);
 
-  let callEl = makeCallElement(call, id);
-
-  $('#inspect').append(detailViewFragment);
-  $('#layers').append(layerContainerEl);
-  $('#calls').append(callEl);
-  selectCall(callEl);
+  selectCall($(`#calls [id="${id}"]`));
 }
-function makeCallElement(call, id) {
-  let el = makeEl('div', id, ['call', call.type]);
+
+function processCall(id, call) {
+  call.id = id;
+  call.layers = Object.fromEntries(Object.entries(call.layers).map(processLayer));
+  call.events = processEvents(call.layers.dataLayer);
+  return call;
+}
+function processLayer([id, layer]) {
+  let keyPairs = Object.fromEntries(Object.entries(layer).map(processKeyPair));
+  let processed = Object.fromEntries(Object.entries(config.keyGroups).map(([groupName, groupDefinition]) => {
+    let definition = { visibility: groupDefinition.hidden ? 'hidden' : 'visible' };
+    definition.keys = Object.fromEntries(groupDefinition.keys.map(key => [key, keyPairs[key] || { value: keyPairs[key], status: 'nokey' }]));
+    if (Object.values(definition.keys).some(value => value.status !== 'nokey')) definition.status = 'ok';
+    else {
+      definition.status = 'nokeys';
+      definition.visibility = 'hidden';
+    }
+    return [groupName, definition];
+  }));
+  return [id, processed];
+}
+function processKeyPair([key, value]) {
+  let status;
+  if (value === null) status = 'missing';
+  else if (Array.isArray(value)) status = value.length > 0 ? 'array' : 'empty';
+  else {
+    let type = typeof value;
+    if (type === 'undefined') status = 'missing';
+    else if (type === 'object') {
+      status = Object.keys(value).length > 0 ? 'object' : 'empty';
+    } else {
+      status = type === 'string' && value.length === 0 ? 'empty' : type;
+    }
+  }
+  return [key, { value: value, status: status }]
+}
+function processEvents(dataLayer) {
+  if (config.events.isGroup) {
+    let events = dataLayer[config.events.key].keys;
+    if (events) return Object.values(events).reduce((list, event) => Array.isArray(event) ? [...list, ...event.value] : [...list, event.value], []);
+  }
+  else {
+    for (let group in dataLayer) {
+      let key = dataLayer[group][config.events.key];
+      if (key.value) return Array.isArray(key.value) ? key.value : [key.value];
+    }
+  }
+  return [];
+}
+
+function createCall(call) {
+  let el = makeEl('div', call.id, ['call', call.type]);
   el.append(makeEl('h2', null, null, call.type));
-  getEvents(call.layers.dataLayer).forEach(text => {
+  call.events.forEach(text => {
     el.append(makeEl('div', null, null, text));
   });
-  return el;
+  $('#calls').append(el);
 }
-
-function makeLayerElement(id, callId) {
-  let el = makeEl('div', null, ['layer'], id);
-  el.dataset.layerId = id;
-  el.dataset.callId = callId;
-  return el;
-}
-
-function makeDetailElement(layer, layerId, callId) {
-  let el = makeEl('div', null, ['detail']);
-  el.dataset.layerId = layerId;
-  el.dataset.callId = callId;
-
-  Object.entries(layer).forEach(([groupName, group]) =>  {
-    groupEl = makeEl('div', null, [groupName]);
-    groupEl.append(makeEl('h2', null, null, groupName));
-    Object.entries(group).forEach(([key, value]) => {
-      groupEl.append(makeEl('div', null, null, `${key}: ${value}`));
-    });
-    el.append(groupEl);
+function createLayers(call) {
+  let container = makeEl('div', null, ['layersContainer']);
+  container.dataset.callId = call.id;
+  Object.keys(call.layers).forEach(layerId => {
+    let el = makeEl('div', null, ['layer'], layerId);
+    el.dataset.layerId = layerId;
+    el.dataset.callId = call.id;
+    container.append(el);
   });
-
-  return el;
+  $('#layers').append(container);
 }
+function createDetails(call) {
+  let fragment = document.createDocumentFragment();
+  Object.entries(call.layers).forEach(([layerId, layer]) => {
+    let el = makeEl('div', null, ['detail']);
+    el.dataset.layerId = layerId;
+    el.dataset.callId = call.id;
 
+    let head = makeEl('div', null, ['head']);
+    let body = makeEl('div', null, ['body']);
+    Object.entries(layer).forEach(([groupName, group]) => {
+      head.append(makeEl('div', null, null, groupName));
+      let content = makeEl('div', null, [group.visibility])
+      content.append(makeEl('h2', null, null, groupName));
+      Object.entries(group.keys).forEach(([key, valueObj]) => {
+        content.append(makeEl('div', null, null, `${key}: ${valueObj.value}`));
+      });
+      body.append(content);
+    });
+    el.append(head);
+    el.append(body);
+    fragment.append(el);
+  })
+  $('#inspect').append(fragment);
+}
 function makeEl(type, id, classes, text) {
   var el = document.createElement(type);
   if (id || id === 0) el.id = id;
   if (classes) classes.forEach(c => el.classList.add(c));
   if (text !== null && text !== undefined) el.append(document.createTextNode(text));
   return el;
-}
-
-function getEvents(dataLayer) {
-  if (config.events.isGroup) {
-    let events = dataLayer[config.events.key];
-    if (events) return Object.values(events).reduce((list, event) => Array.isArray(event) ? [...list, ...event] : [...list, event], []);
-  }
-  else {
-    for (let group in dataLayer) {
-      let key = dataLayer[group][config.events.key];
-      if (key) return Array.isArray(key) ? key : [key];
-    }
-  }
-  return [];
 }
 
 function selectCall(el) {
